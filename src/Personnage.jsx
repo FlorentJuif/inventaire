14b9b467fdb781c7a158407589b6cbe240219087
Florent JUIF <florentjuif@gmail.com>
2020-03-04 10:35:52 +0100

    useEffect total des armes

--- src/Personnage.jsx
+++ src/Personnage.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 import Armurie from "./armurie";
 import Stuff from "./Stuff";
+import { useEffect } from "react";
 
 const Personnage = () => {
-  const [armes, setArmes] = useState([
-    {
-      nom: "Ak",
-      image:
-        "https://lh5.googleusercontent.com/proxy/besdPgtzQWvsO3vPL9qRO4uhpEXdiR78U9p0zqqRq9q8faAyFu2b_bGv0ybm-KGM_EkWWunsBjHG02YyACDBsG1YlOnHJWOYN3IA"
-    },
-    {
-      nom: "m4",
-      image: "https://csgotestout.com/images/weapons/M4A4%20Asiimov.png"
-    },
-    { nom: "USP", image: "https://gocase.pro/assets/weapon2/usp.png" },
-    {
-      nom: "Glock",
-      image:
-        "https://steamcdn-a.akamaihd.net/apps/730/icons/econ/default_generated/weapon_glock_cu_glock_wasteland_rebel_light_large.284899ab35e5a29c6edb64b2af194cee19a0ed89.png"
-    }
-  ]);
+  const [armes, setArmes] = useState([]);
+
+  useEffect(() => {
+    fetch("/weapons")
+      .then(info => {
+        return info.json();
+      })
+      .then(data => {
+        const apiArmes = data.map(({ name, imageUrl }) => {
+          return {
+            name,
+            image:imageUrl
+          };
+        });
+        setArmes(apiArmes);
+      });
+  }, []);
 
   const [armeDroite, setArmeDroite] = useState();
   const [armeCeinture, setArmeCeinture] = useState();
@@ -36,23 +37,20 @@ const Personnage = () => {
             setArmes(armes.filter(restant => restant !== quelArme)); // suprime l'arme de l'inventaire quand posée sur MD
             setQuelArme(undefined); // une fois l'amre mise, le clic n'a plus d'arme
           } else {
-            let newArmes=[...armes,armeDroite]
-            setArmes(newArmes.filter(restant => restant !== quelArme))
-            setArmeDroite(quelArme)
-            setQuelArme(undefined)
+            let newArmes = [...armes, armeDroite];
+            setArmes(newArmes.filter(restant => restant !== quelArme));
+            setArmeDroite(quelArme);
+            setQuelArme(undefined);
           }
         }}
         onClickMg={() => {
-          if (armeGauche === undefined) {
-            setArmeGauche(quelArme);
-            setArmes(armes.filter(restant => restant !== quelArme));
-            setQuelArme(undefined);
-          } else {
-            let newArmes=[...armes,armeGauche]
-            setArmes(newArmes.filter(restant=>restant!==quelArme))
-            setArmeGauche(quelArme)
-            setQuelArme(undefined)
+          let newArmes = armes;
+          if (armeGauche !== undefined) {
+            newArmes = [...armes, armeGauche];
           }
+          setArmes(newArmes.filter(restant => restant !== quelArme));
+          setArmeGauche(quelArme);
+          setQuelArme(undefined);
         }}
         onClickCeinture={() => {
           if (armeCeinture === undefined) {
@@ -60,10 +58,10 @@ const Personnage = () => {
             setArmes(armes.filter(restant => restant !== quelArme));
             setQuelArme(undefined);
           } else {
-          let newArmes=[...armes,armeCeinture]
-          setArmes(newArmes.filter(restant=>restant!==quelArme))
-          setArmeCeinture(quelArme)
-          setQuelArme(undefined)
+            let newArmes = [...armes, armeCeinture];
+            setArmes(newArmes.filter(restant => restant !== quelArme));
+            setArmeCeinture(quelArme);
+            setQuelArme(undefined);
           }
         }}
         armeDroite={armeDroite}
