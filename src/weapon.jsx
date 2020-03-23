0c5ca50b9734fc67b4292c08e46eacd151a6c2e9
Florent JUIF <florentjuif@gmail.com>
2020-03-23 11:52:16 +0100

    props en dur

--- src/weapon.jsx
+++ src/weapon.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Weapon=({nom,image,onClick})=>{
+const Weapon=({nom,image})=>{
+    let nom='Ak'
+    let image='https://www.destockage-games.com/catalogue-fusil-mitrailleur-kalashnikov-ak47-aeg-electrique-kalachnikov-ak-47-120903/image/3559961209030_000_kalashnikov_ak47_crosse_pleine_340bbs_aeg_0_8j_120903_.jpg.webp'
     return(
-        <div onClick={onClick}>
+        <div >
             {nom}
            <img src={image}/>
-            Je suis une arme
+            
         </div>
     )
 }
