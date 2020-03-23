6709c7aadb89325d58c93495a692d6312b30fd38
Florent JUIF <florentjuif@gmail.com>
2020-03-23 11:54:42 +0100

    instancier 2 fois dans App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Weapon from './weapon'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Weapon/>
+      <Weapon/>
       
     </div>
   );
--- src/weapon.jsx
+++ src/weapon.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Weapon=({nom,image})=>{
+const Weapon=()=>{
     let nom='Ak'
     let image='https://www.destockage-games.com/catalogue-fusil-mitrailleur-kalashnikov-ak47-aeg-electrique-kalachnikov-ak-47-120903/image/3559961209030_000_kalashnikov_ak47_crosse_pleine_340bbs_aeg_0_8j_120903_.jpg.webp'
     return(
