f6a45bd76a2d06ac8839f15a47daebbfde4f0a2d
Florent JUIF <florentjuif@gmail.com>
2020-03-26 13:57:37 +0100

    destructuring

--- src/Armurerie.jsx
+++ src/Armurerie.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Weapon from "./weapon";
 
-const Armory = ({props}) => {
-  let weapons = props.weapons;
+const Armory = ({weapons,onClick}) => {
+
 
   return (
     <div className="armurerie">
       {weapons.map(weapon => (
         <Weapon key={weapon.name} name={weapon.name} picture={weapon.name} />
       ))}
-      <button onClick={props.onClick}>Add Weapon</button>
+      <button onClick={onClick}>Add Weapon</button>
     </div>
   );
 };
